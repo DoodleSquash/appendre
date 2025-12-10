7f61bf7ca58d68f5f751c8ef916fcd5d62e80f3c
DoodleSquash <doodlesquash2006@gmail.com>
2025-12-11 01:48:59 +0530

    chore: update dependencies and configure Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <div className="text-4xl text-blue-500 font-bold p-4">
+  Tailwind is working 🎉
+</div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
